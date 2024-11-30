d7b020b557b03613cd110e47d17a7fba7e1262c2
Brandt Green <61711534+Brandt-moreThan4@users.noreply.github.com>
2024-11-30 13:54:55 -0600

    unhardcode url link in javascript

--- frontend/script.js
+++ frontend/script.js
@@ -48,7 +48,7 @@ function handleCellClick(event) {
 async function getBotMove(board) {
     try {
         // Make a request to the backend to get the bot's move
-        const response = await fetch('http://127.0.0.1:5000/api/move', {
+        const response = await fetch('api/move', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
